09e3b9fcb9e2ccef9faa453392a13cf249236792
highlaw00 <choiyool00@gmail.com>
2022-10-25 15:32:24 +0900

    This commitment is for test.

---
+++ app/src/main/java/com/example/workoutapplication/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.workoutapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.workoutapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    private fun updateUi() {
+        binding.clickMe.text = "For Test"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.clickMe.setOnClickListener{
+            this.updateUi()
+        }
+    }
+}
