eb098aac832a6c80a78c40962471ae6af94730c1
Highlaw <choiyool00@gmail.com>
2022-11-04 22:31:27 +0900

    Added RecyclerView in Fragment. Added tabLayout to Fragment.

--- app/src/main/java/com/example/workoutapplication/PreparationActivity.kt
+++ app/src/main/java/com/example/workoutapplication/PreparationActivity.kt
@@ -10,6 +10,12 @@ import com.google.android.material.tabs.TabLayout
 
 class PreparationActivity : AppCompatActivity() {
 
+    // TODO: 만약 리스트가 비어있다면... 비어있는 scroll view 를 출력하도록 합니다.
+    // TODO: REMOVE 버튼을 삭제하고, Floating Button으로 ADD 와 START만 구현합니다.
+    // TODO: 단, REMOVE는 리사이클러 뷰 안의 컴포넌트를 스와이프할 시 삭제할 수 있도록 합니다.
+    // TODO: 추가로, DRAG하여 순서를 조작할 수 있게 하며, TOUCH 시 SET 정보(중량과 세트 횟수)를 기록할 수 있도록 합니다.
+
+
     object List {
         var workoutList = arrayListOf(
             Exercise(R.drawable.bench_press, "Bench Press", 80, "Chest"),
@@ -36,8 +42,10 @@ class PreparationActivity : AppCompatActivity() {
         preBinding = ActivityPreparationBinding.inflate(layoutInflater)
         setContentView(preBinding.root)
 
+        // 초기 Fragment는 Workout Fragment로 고정해주기 위한 함수
         initFragmentUi()
 
+        // Tab 버튼 눌렀을 때 Fragment 전환해주는 Listener
         preBinding.tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab?) {
                 val transaction = supportFragmentManager.beginTransaction()
