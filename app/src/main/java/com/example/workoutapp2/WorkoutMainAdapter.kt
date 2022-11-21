3dffb645c7c40ef3b7ad09c097b1e217584333f6
Highlaw <choiyool00@gmail.com>
2022-11-22 01:48:37 +0900

    WorkoutMainAdapter.kt gets livedata

--- app/src/main/java/com/example/workoutapp2/WorkoutMainAdapter.kt
+++ app/src/main/java/com/example/workoutapp2/WorkoutMainAdapter.kt
@@ -7,28 +7,30 @@ import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.RecyclerView
 import com.example.workoutapp2.databinding.ListWorkoutMainBinding
 
-class WorkoutMainAdapter(var data: List<Exercise>) : RecyclerView.Adapter<WorkoutMainAdapter.Holder>() {
+class WorkoutMainAdapter(private val data: LiveData<ArrayList<Exercise>>) : RecyclerView.Adapter<WorkoutMainAdapter.Holder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val binding = ListWorkoutMainBinding.inflate(LayoutInflater.from(parent.context),parent, false)
         return Holder(binding)
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
-        val exercise = data[position]
-        with (holder) {
-            name?.text = exercise.name
-            part?.text = exercise.part
-            img?.setImageResource(exercise.img)
+        val exercise = data.value?.get(position)
+        if (exercise != null) {
+            with (holder) {
+                name.text = exercise.name
+                part.text = exercise.part
+                img.setImageResource(exercise.img)
+            }
         }
     }
 
     override fun getItemCount(): Int {
-        return data.size
+        return data.value?.size ?: 0
     }
 
     class Holder(private val binding: ListWorkoutMainBinding): RecyclerView.ViewHolder(binding.root) {
-        var img = binding?.ivWorkoutImageMain
-        var name = binding?.tvWorkoutNameMain
-        var part = binding?.tvWorkoutDescMain
+        var img = binding.ivWorkoutImageMain
+        var name = binding.tvWorkoutNameMain
+        var part = binding.tvWorkoutDescMain
     }
 }
